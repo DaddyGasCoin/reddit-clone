c57f5a2e4ff282436b245a194040bc4018f3ba23
Michael Jones <iammichaeljon27@gmail.com>
2022-09-17 22:10:15 +0400

    Add link and thumbnail for foreign linked posts

--- src/components/DisplayPost/DisplayPost.js
+++ src/components/DisplayPost/DisplayPost.js
@@ -10,6 +10,14 @@ const DisplayPost = (props) => {
   const formated_upvotes = Intl.NumberFormat('en', { notation: 'compact' }).format(upvotes)
   const diff = new Date() - new Date(time * 1000)
   const formatedTime = formater(diff)
+
+
+  let outBoundLink = false;
+  if (img) {
+    if (!img.includes('jpg')) {
+      outBoundLink = true
+    }
+  }
   return (
 
     <div className="post-wrapper">
@@ -27,20 +35,37 @@ const DisplayPost = (props) => {
         </div>
       </div>
       <div className="post-content">
-        <div className="post-info-wrap">
-          {/* Render subname if frontpage */}
-          {sub ? <div className="subname">
-            r/{sub}
-          </div> : null}
-          <div className="author-text">
-            Posted by u/{author} {formatedTime}
+        <div className='test-wrap'>
+          <div className='column-wrap'>
+            <div className="post-info-wrap">
+              {/* Render subname if frontpage */}
+              {sub ? <div className="subname">
+                r/{sub}
+              </div> : null}
+              <div className="author-text">
+                Posted by u/{author} {formatedTime}
+              </div>
+            </div>
+            <div className="post=title">
+              {title}
+            </div>
+            {/* display thumbnail and link if foreign content */}
+            {outBoundLink ? <a href={img} target="_blank" rel="noopener noreferrer" > {img} </a> : null}
           </div>
-        </div>
-        <div className="post=title">
-          {title}
+          {outBoundLink ? <img src={thumbnil} className="thumb-small" alt='content-thumbnail' /> : null}
         </div>
         <div className='thumb-wrapper'>
-          {img ? <img src={img} className="thumbnail" /> : null}
+          {
+            (() => {
+              if (!img)
+                return null
+              if (!outBoundLink)
+                return <img src={img} className="thumbnail" alt='content' />
+              else
+                return null
+            })()
+          }
+
           <div className="link-wrapper">
             <Link style={{ textDecoration: 'none' }} to={`/comments/${[Object.keys(posts)]}`}>
               <div className="link-box">
@@ -79,4 +104,16 @@ export default DisplayPost
 
 
 
+
+      // {
+      //   (() => {
+      //     if (!img)
+      //       return null
+      //     // return null while fetching data
+      //     if (!outBoundLink)
+      //       return <img src={img} className="thumbnail" />
+      //     else
+      //       return null
+      //   })()
+      // }
 
--- src/components/DisplayPostWithComments/DisplayPostBig.js
+++ src/components/DisplayPostWithComments/DisplayPostBig.js
@@ -15,81 +15,85 @@ import { orderBy } from 'lodash'
 //Displays single post including contents and comments 
 const DisplayPostBig = (props) => {
 
-   const { data, sub } = props
-   const { author, img, comment, content, time, title, upvotes } = data
-   const formated_upvotes = Intl.NumberFormat('en', { notation: 'compact' }).format(upvotes)
-   const diff = new Date() - new Date(time * 1000)
-   const formatedTime = formater(diff)
-   const [comments, setComments] = useState(comment)
-   const [rules, setRules] = useState()
-   const best = comment
+  const { data, sub } = props
+  const { author, img, comment, content, time, title, upvotes, thumbnil } = data
+  const formated_upvotes = Intl.NumberFormat('en', { notation: 'compact' }).format(upvotes)
+  const diff = new Date() - new Date(time * 1000)
+  const formatedTime = formater(diff)
+  const [comments, setComments] = useState(comment)
+  const [rules, setRules] = useState()
+  const best = comment
 
-   function sortComments(event, option) {
-      if (option === 'best') {
-         setComments(best)
-         return
-      }
-      const sortedComments = orderBy(comments, ['upvotes'], [option])
-      setComments(sortedComments)
-   }
-   useEffect(() => {
-      async function getCommentsFromDB() {
-         if (sub.includes('front')) {
-            setRules('front')
-            return
-         }
-         const str = sub.substring(0, sub.indexOf('-'))
-         const docname = `${str}-rules`.toLowerCase()
-         console.log(docname)
-         const docRef = doc(db, docname, 'rules');
-         const docSnap = await getDoc(docRef);
-         setRules(docSnap.data().data)
-      }
-      getCommentsFromDB()
-   }, [])
+  let outBoundLink = false;
+  if (img) {
+    if (!img.includes('jpg')) {
+      outBoundLink = true
+    }
+  }
 
-   return (
-      <div className="row-wrapper">
-         <div className="content-wrapper-big">
-            <div className="vote-content">
-               <DisplayVoteContent votes={formated_upvotes} />
-            </div>
-            <div className='post-content'>
-               <DisplayPostInfo data={[author, title, formatedTime]} />
-               <img src={img} />
-               <DisplayIconBar />
-               <DisplayCommentSort handler={sortComments} />
-               {comments.map((x) => {
-                  return <DisplayComments data={x} key={x.id} />
-               })}
-            </div>
-         </div>
-         {/* {rules ? <DisplayRules data={rules} /> : <DisplaySidebar />} */}
-         {
+  function sortComments(event, option) {
+    if (option === 'best') {
+      setComments(best)
+      return
+    }
+    const sortedComments = orderBy(comments, ['upvotes'], [option])
+    setComments(sortedComments)
+  }
+  useEffect(() => {
+    async function getCommentsFromDB() {
+      if (sub.includes('front')) {
+        setRules('front')
+        return
+      }
+      const str = sub.substring(0, sub.indexOf('-'))
+      const docname = `${str}-rules`.toLowerCase()
+      const docRef = doc(db, docname, 'rules');
+      const docSnap = await getDoc(docRef);
+      setRules(docSnap.data().data)
+    }
+    getCommentsFromDB()
+  }, [])
+
+  return (
+    <div className="row-wrapper">
+      <div className="content-wrapper-big">
+        <div className="vote-content">
+          <DisplayVoteContent votes={formated_upvotes} />
+        </div>
+        <div className='post-content'>
+          <DisplayPostInfo data={[author, title, formatedTime]} />
+          {
             (() => {
-               if (rules === 'front')
-                  return <DisplaySidebar />
-               if (rules)
-                  return <DisplayRules data={rules} />
-               else
-                  return null
+              if (!img)
+                return null
+              if (!outBoundLink)
+                return <img src={img} alt='content' />
+              else
+                return <img src={thumbnil} className="outbound-content" alt='content-thumbnail' />
             })()
-         }
-
-
+          }
+          {outBoundLink ? <a href={img} target="_blank" rel="noopener noreferrer">
+            {img} </a> : null}
+          <DisplayIconBar />
+          <DisplayCommentSort handler={sortComments} />
+          {comments.map((x) => {
+            return <DisplayComments data={x} key={x.id} />
+          })}
+        </div>
       </div>
-   )
+      {
+        (() => {
+          if (rules === 'front')
+            return <DisplaySidebar />
+          if (rules)
+            return <DisplayRules data={rules} />
+          else
+            return null
+        })()
+      }
+    </div>
+  )
 }
 
 export default DisplayPostBig
 
-// {
-//    (() => {
-//        if (rules === 'front')
-//           return <DisplaySidebar/>
-//        if (rules)
-//           return <DisplayRules data={rules}/>
-//        else 
-//           return null
-//    })()
-// }
